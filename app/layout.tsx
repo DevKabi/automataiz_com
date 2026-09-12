2cde1018ec6a99891b798955da0b8f1ff9ad785b
Debasish Kabi <hello@automataiz.com>
2026-09-12 23:24:07 +0530

    feat: install Google tag (gtag.js) G-SF2V2QDXVR in root layout header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Outfit, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
@@ -83,6 +84,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${outfit.variable} ${jetbrainsMono.variable} dark`}>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-SF2V2QDXVR"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SF2V2QDXVR');
+            `,
+          }}
+        />
+      </head>
       <body className="bg-[#050505] text-white min-h-screen flex flex-col relative selection:bg-brand-green selection:text-black">
         {/* Top Scroll Laser Beam */}
         <ScrollProgress />
